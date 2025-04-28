286738ba61f41698eef0f569370ff76745e3b1c7
nerea <nereacandia5@gmail.com>
2025-04-28 15:44:53 -0300

    rutas simples

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/saludo")
+def Hola_chau():
+    return "<p>Hola, chau!</p>"
+
+@app.route("/partido") 
+def River_Boca():
+    return "<p> River, Boca!</p>"
